35e804235d4aff305598dd19df180e17455bd965
paullaster <paullasterokoth98@gmail.com>
2022-09-05 21:16:16 +0300

    proportion of co name starting with vowels

--- exploring_propotion_of_text_to_num_in_init_let_of_co.sql
+++ exploring_propotion_of_text_to_num_in_init_let_of_co.sql
@@ -16,3 +16,24 @@ FROM(
             IN('0', '1','2','3','4','5','6','7','8','9' )
             THEN 0 ELSE 1 END AS init_letter
        FROM accounts)t1
+
+/*
+Consider vowels as a, e, i, o, and u. 
+What proportion of company names start with
+ a vowel, and what percent start with anything else?
+*/
+SELECT 
+    SUM(init_vowel)vowels, 
+    SUM(init_other_than_vowel)other_letters
+FROM(
+  	  SELECT name,
+	   LEFT(name, 1) f_letter,
+       CASE
+       WHEN LEFT(name, 1)
+       IN ('A','E','I','O','U')
+       THEN 1 ELSE 0 END AS init_vowel,
+       CASE
+       WHEN LEFT(name, 1)
+       IN('A','E','I','O','U')
+       THEN 0 ELSE 1 END AS init_other_than_vowel
+       FROM accounts)t1

38b09b0c379ada78d1ae9541b94fb1bd346d7a90
paullaster <paullasterokoth98@gmail.com>
2022-09-06 07:58:53 +0300

    selecting dates

--- date_cleaning.sql
+++ date_cleaning.sql
@@ -2,9 +2,16 @@
 Writing dates stored from crime scene, 
 into SQL format
 */
-WITH t1 AS (SELECT date,
-LTRIM(REPLACE(SUBSTRING(date, 1,2), '0', ''), '') AS month,
-SUBSTRING(date, 4,2) AS day,
-SUBSTRING(date, 7,4) AS year
-FROM sf_crime_data
-LIMIT 10)
+WITH t1 AS (
+    SELECT date,
+        LTRIM(REPLACE(SUBSTRING(date, 1,2), '0', ''), '') AS month,
+        SUBSTRING(date, 4,2) AS day,
+        SUBSTRING(date, 7,4) AS year
+        FROM sf_crime_data
+        LIMIT 10
+        )
+
+SELECT 
+date,
+CAST(year||'-'||month||'-'||day AS date) AS clean_date
+FROM t1
